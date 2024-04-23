426c5573f67526bd01d22d93b3c86fbc531d3c78
Nishikant Singh <nishikantsingh2004@gmail.com>
2024-04-23 09:22:50 +0530

    added carousel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,13 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className=" w-full flex flex-col justify-between ">
-      <div className=" w-full p-2 flex justify-between items-center px-4">
+      <div className=" w-full p-2 flex justify-between items-center px-24">
         <Image src={image} width={150} alt="Logo" />
-        <h1 className=" text-2xl font-bold text-neutral-600">Contact us - xyz@email.com</h1>
+        <h1 className=" text-2xl font-bold text-neutral-600">
+            Contact us - xyz@email.com
+        </h1>
       </div>
-      <div className="w-full bg-[#d8cbc4] flex justify-end gap-6 text-lg font-semibold px-4 py-2">
+      <div className="w-full bg-[#d8cbc4] flex justify-end gap-6 text-lg font-semibold py-2 pr-24">
         <Link href={"/"}>Home</Link>
         <Link href={"/about"}>About us</Link>
         <Link href={"/departments"}>Departments</Link>
